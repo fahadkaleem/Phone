f412862d8a22b11ee6a057950578750e5f8dcf61
Mohammed Fahad Kaleem <mohammedfahadkaleem@gmail.com>
2018-03-21 00:39:00 -0500

    + Added Model Class for Contact + Added dummy data + Added logic for Favourites Fragment

--- app/src/main/java/com/mohammedfahadkaleem/phone/MainActivity.java
+++ app/src/main/java/com/mohammedfahadkaleem/phone/MainActivity.java
@@ -1,14 +1,49 @@
 package com.mohammedfahadkaleem.phone;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.mohammedfahadkaleem.phone.adapter.ViewPagerAdapter;
+import com.mohammedfahadkaleem.phone.fragment.ContactsListFragment;
+import com.mohammedfahadkaleem.phone.fragment.FavouritesFragment;
+import com.mohammedfahadkaleem.phone.fragment.RecentsFragment;
+import com.mohammedfahadkaleem.phone.fragment.VoicemailFragment;
 
 public class MainActivity extends AppCompatActivity {
+  @BindView(R.id.view_pager)
+  ViewPager mViewPager;
+
+  @BindView(R.id.tab_layout)
+  TabLayout mTabLayout;
+
+  private ViewPagerAdapter mViewPagerAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
+    setupTabLayout();
+  }
 
+  private void setupTabLayout(){
+    setupViewPager(mViewPager);
+    mTabLayout.setupWithViewPager(mViewPager);
+    mTabLayout.getTabAt(0).setIcon(R.drawable.ic_star);
+    mTabLayout.getTabAt(1).setIcon(R.drawable.ic_recents);
+    mTabLayout.getTabAt(2).setIcon(R.drawable.ic_contacts_list);
+    mTabLayout.getTabAt(3).setIcon(R.drawable.ic_voicemail);
+  }
+
+  private void setupViewPager(ViewPager viewPager){
+    mViewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+    mViewPagerAdapter.addFragment(FavouritesFragment.newInstance());
+    mViewPagerAdapter.addFragment(RecentsFragment.newInstance());
+    mViewPagerAdapter.addFragment(ContactsListFragment.newInstance());
+    mViewPagerAdapter.addFragment(VoicemailFragment.newInstance());
+    mViewPager.setAdapter(mViewPagerAdapter);
   }
 }
--- app/src/main/java/com/mohammedfahadkaleem/phone/fragment/FavouritesFragment.java
+++ app/src/main/java/com/mohammedfahadkaleem/phone/fragment/FavouritesFragment.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.mohammedfahadkaleem.phone.R;
 
 /**
@@ -14,6 +18,9 @@ import com.mohammedfahadkaleem.phone.R;
  */
 
 public class FavouritesFragment extends Fragment{
+ @BindView(R.id.favourites_recycler_view)
+ RecyclerView mRecyclerView;
+
  public FavouritesFragment(){}
 
  public static FavouritesFragment newInstance(){
@@ -23,9 +30,11 @@ public class FavouritesFragment extends Fragment{
 
   @Nullable
   @Override
-  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-      @Nullable Bundle savedInstanceState) {
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
    View view = inflater.inflate(R.layout.fragment_favourites,container,false);
+   ButterKnife.bind(getActivity());
+   mRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(),2));
+   mRecyclerView.setHasFixedSize(true);
     return view;
   }
 }
